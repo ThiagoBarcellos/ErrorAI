f973a194593e70edcc8bae4da407ac471e813568
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2017-06-30 21:41:13 -0300

    Seleção 1.0
    Esboço script seleção de personagem 1.0

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : NetworkBehaviour {
+
+	NetworkIdentity NI = GetComponent<NetworkIdentity>();
+
+	void Start () {
+		//NI.assetId = 1;
+	}
+	
+	void Update () {
+		/*switch (NI.assetId) {
+		case 1:
+			{
+				break;	
+			}
+		}*/
+	}
+}
